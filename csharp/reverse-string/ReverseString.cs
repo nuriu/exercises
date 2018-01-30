78ac5972ba4f74a67aaaa25e471e128e7bc68b7a
Nuri UZUNOĞLU <nuriu@protonmail.com>
2018-01-30 12:42:26 +0300

    added first 3 c# exercises.

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        var inputArray = input.ToCharArray();
+        Array.Reverse(inputArray);
+        return new string(inputArray);
+    }
+}

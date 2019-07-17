53a19bc3e61c128cf9bfb3330dffff9c0f08ee9b
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-07-17 13:50:11 +0300

    added csharp/two-fer solution.

---
+++ csharp/two-fer/TwoFer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class TwoFer
+{
+    public static string Speak(string name = "")
+    {
+        return "One for " + (name != "" ? name : "you") + ", one for me.";
+    }
+}

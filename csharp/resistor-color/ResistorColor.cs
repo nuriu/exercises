3987a0629088fc622434a024b52c4ef76e4492e6
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-11-02 12:09:46 +0300

    added csharp/resistor-color solution.

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    private static string[] _colors = new string[] {
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+    };
+
+    public static int ColorCode(string color)
+    {
+        return Array.FindIndex(_colors, cv => cv == color.ToLowerInvariant());
+    }
+
+    public static string[] Colors()
+    {
+        return _colors;
+    }
+}

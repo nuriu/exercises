fa7266c3bd3524ab4588181a0bca851d52ba5678
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-11-06 19:14:20 +0300

    updated go/hamming solution.

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,15 +1,17 @@
 package hamming
 
-import "github.com/pkg/errors"
+import "errors"
 
 func Distance(a, b string) (int, error) {
-	if len(a) != len(b) {
-		return -1, errors.New("lengths of a and b must be equal")
+	ar, br := []rune(a), []rune(b)
+
+	if len(ar) != len(br) {
+		return 0, errors.New("lengths of a and b must be equal")
 	}
 
 	dist := 0
-	for i := 0; i < len(a); i++ {
-		if a[i] != b[i] {
+	for i := 0; i < len(ar); i++ {
+		if ar[i] != br[i] {
 			dist++
 		}
 	}

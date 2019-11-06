15cd716f268b3725c0fd123049eb0166247d4b0c
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-11-06 13:56:34 +0300

    added csharp/bob solution.

---
+++ csharp/bob/Bob.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Bob
+{
+    private enum STATEMENT_TYPES
+    {
+        SILENCE,
+        YELLING,
+        QUESTIONING,
+        YELLING_AND_QUESTIONING,
+        SOMETHING_ELSE
+    }
+
+    private static Dictionary<STATEMENT_TYPES, string> ANSWERS = new Dictionary<STATEMENT_TYPES, string>()
+    {
+        {STATEMENT_TYPES.SILENCE, "Fine. Be that way!"},
+        {STATEMENT_TYPES.YELLING, "Whoa, chill out!"},
+        {STATEMENT_TYPES.QUESTIONING, "Sure."},
+        {STATEMENT_TYPES.YELLING_AND_QUESTIONING, "Calm down, I know what I'm doing!"},
+        {STATEMENT_TYPES.SOMETHING_ELSE, "Whatever."}
+    };
+
+    public static string Response(string statement)
+    {
+        return ANSWERS[ClassificateStatement(statement)];
+    }
+
+    private static STATEMENT_TYPES ClassificateStatement(string statement)
+    {
+        if (string.IsNullOrWhiteSpace(statement))
+        {
+            return STATEMENT_TYPES.SILENCE;
+        }
+
+        var isYelling = statement.Trim().Any(char.IsLetter) && string.CompareOrdinal(statement.ToUpper(), statement) == 0;
+        var isQuestioning = statement.Trim().Last() == '?';
+
+        if (isYelling && !isQuestioning)
+        {
+            return STATEMENT_TYPES.YELLING;
+        }
+
+        if (!isYelling && isQuestioning)
+        {
+            return STATEMENT_TYPES.QUESTIONING;
+        }
+
+        if (isYelling && isQuestioning)
+        {
+            return STATEMENT_TYPES.YELLING_AND_QUESTIONING;
+        }
+
+        return STATEMENT_TYPES.SOMETHING_ELSE;
+    }
+}

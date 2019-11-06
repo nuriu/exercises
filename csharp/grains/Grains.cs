719c04b59aa4f60aa6a67cdc1969d57cc8f1dc8e
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-11-06 19:34:46 +0300

    added csharp/grains solution.

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if (n < 1 || n > 64)
+            throw new ArgumentOutOfRangeException();
+
+        return (ulong)Math.Pow(2, n - 1);
+    }
+
+    public static ulong Total() => Enumerable.Range(1, 64).Select(Square).Aggregate((a, b) => a + b);
+}

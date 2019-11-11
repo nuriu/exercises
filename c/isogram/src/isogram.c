b83849c5b09116b2f14728797195c6595a267af5
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-11-11 15:52:10 +0300

    reformatted c/isogram source.

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -9,11 +9,11 @@ bool is_isogram(const char phrase[])
 
     int i = 0, j = 0;
 
-    while(phrase[i] != '\0')
+    while (phrase[i] != '\0')
     {
         j = i + 1;
 
-        while(phrase[j] != '\0')
+        while (phrase[j] != '\0')
         {
             if (phrase[i] != ' ' && phrase[i] != '-' && tolower(phrase[i]) == tolower(phrase[j]))
             {

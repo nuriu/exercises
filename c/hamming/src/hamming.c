69194166360fde3f995d6eaec681fd0052b60276
Nuri Uzunoğlu <nuriu@protonmail.com>
2019-11-13 22:06:30 +0300

    fixed c/hamming exercise solution.

--- c/hamming/src/hamming.c
+++ c/hamming/src/hamming.c
@@ -1,9 +1,17 @@
 #include "hamming.h"
 
+#include <string.h>
+
 // calculates hamming distance between two dna strands
 int compute(const char* first_strand, const char* second_strand)
 {
     int distance = 0;
+
+    if (first_strand == NULL || second_strand == NULL)
+        return -1;
+
+    if (strlen(first_strand) != strlen(second_strand))
+        return -1;
 
     while (*first_strand && *second_strand)
     {
